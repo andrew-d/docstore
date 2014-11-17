69202fe34c74ec6c19c1ddccfbf7f1f94f74798c
Andrew Dunham <andrew@du.nham.ca>
2014-11-16 21:56:21 -0800

    Simplify config.py

--- config.py
+++ config.py
@@ -3,14 +3,11 @@
 import os
 
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
 CSRF_ENABLED = True
 SECRET_KEY   = ('cb04a4d1284a33e7d2a6cebc864c70acf5112992' +
                 'f50c3a9caf561cd7d013576f1b0e36b129c29b6f')
-DATA_DIRECTORY = basedir
+DATA_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
 
-# Database
 if os.environ.get('DATABASE_URL') is None:
     SQLALCHEMY_DATABASE_URI = ('sqlite:///' + os.path.join(DATA_DIRECTORY, 'app.db') +
                                '?check_same_thread=False')
