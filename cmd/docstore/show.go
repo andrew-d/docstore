f826125b3f9a288a30f42d673c05fd3674d277f1
Andrew Dunham <andrew@du.nham.ca>
2015-04-16 17:12:33 -0700

    Add "show tag" command"

--- cmd/docstore/show.go
+++ cmd/docstore/show.go
@@ -18,10 +18,17 @@ var (
 		Short:   "Show a single document, given either an ID or a name",
 		Run:     wrapCommand(runShowDocument),
 	}
+
+	showTagCmd = &cobra.Command{
+		Use:   "tag <id or name>",
+		Short: "Show a single tag, given either an ID or a name",
+		Run:   wrapCommand(runShowTag),
+	}
 )
 
 func init() {
 	showCmd.AddCommand(showDocumentCmd)
+	showCmd.AddCommand(showTagCmd)
 }
 
 func runShowDocument(cmd *cobra.Command, args []string, store *store.Store) {
@@ -56,4 +63,38 @@ func runShowDocument(cmd *cobra.Command, args []string, store *store.Store) {
 
 	// TODO: print in a real format?
 	json.NewEncoder(os.Stdout).Encode(&d)
+}
+
+func runShowTag(cmd *cobra.Command, args []string, store *store.Store) {
+	if len(args) != 1 {
+		log.Error("Command takes exactly one argument")
+		cmd.Help()
+		return
+	}
+
+	// Is this a number or a string?
+	var (
+		id    uint64
+		t     models.Tag
+		found bool
+		err   error
+	)
+
+	if id, err = strconv.ParseUint(args[0], 10, 64); err != nil {
+		t, found, err = store.GetTagById(int64(id))
+	} else {
+		t, found, err = store.GetTagByName(args[0])
+	}
+
+	if err != nil {
+		log.WithField("err", err).Error("Error fetching tag")
+		return
+	}
+	if !found {
+		log.Info("Tag not found")
+		return
+	}
+
+	// TODO: print in a real format?
+	json.NewEncoder(os.Stdout).Encode(&t)
 }
