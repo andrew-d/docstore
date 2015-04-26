6878405c748f9254cf0dd2949285ecf0a4e4447d
Andrew Dunham <andrew@du.nham.ca>
2015-04-26 02:31:13 -0700

    Add initial query-to-SQL converter

---
+++ query/sql/sql.go
@@ -0,0 +1,48 @@
+package sql
+
+import (
+	"github.com/andrew-d/docstore/query/parse"
+)
+
+func NodesToSQL(root parse.Node, defaultField string) (sql string, args []interface{}) {
+	var traverse func(node parse.Node) (string, []interface{})
+
+	traverse = func(node parse.Node) (string, []interface{}) {
+		switch v := node.(type) {
+		case *parse.TextNode:
+			return defaultField + " = ?", []interface{}{v.Text}
+
+		case *parse.OrNode:
+			leftSql, leftArgs := traverse(v.Left)
+			rightSql, rightArgs := traverse(v.Right)
+
+			sql := "(" + leftSql + ") OR (" + rightSql + ")"
+			args := make([]interface{}, len(leftArgs)+len(rightArgs))
+			copy(args[0:], leftArgs)
+			copy(args[len(leftArgs):], rightArgs)
+
+			return sql, args
+
+		case *parse.AndNode:
+			leftSql, leftArgs := traverse(v.Left)
+			rightSql, rightArgs := traverse(v.Right)
+
+			sql := "(" + leftSql + ") AND (" + rightSql + ")"
+			args := make([]interface{}, len(leftArgs)+len(rightArgs))
+			copy(args[0:], leftArgs)
+			copy(args[len(leftArgs):], rightArgs)
+
+			return sql, args
+
+		case *parse.NotNode:
+			sql, args := traverse(v.Node)
+
+			return "NOT (" + sql + ")", args
+
+		default:
+			panic("unknown node type")
+		}
+	}
+
+	return traverse(root)
+}
---
+++ query/sql/sql_test.go
@@ -0,0 +1,39 @@
+package sql
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/andrew-d/docstore/query/parse"
+)
+
+func TestNodesToSQL(t *testing.T) {
+	testCases := []struct {
+		Desc string
+		SQL  string
+		Args []interface{}
+		Node parse.Node
+	}{
+		{`simple value`, "default = ?", []interface{}{"foo"}, &parse.TextNode{
+			Text: "foo",
+		}},
+		{`simple OR`, "(default = ?) OR (default = ?)", []interface{}{"foo", "bar"}, &parse.OrNode{
+			Left:  &parse.TextNode{Text: "foo"},
+			Right: &parse.TextNode{Text: "bar"},
+		}},
+		{`simple AND`, "(default = ?) AND (default = ?)", []interface{}{"foo", "bar"}, &parse.AndNode{
+			Left:  &parse.TextNode{Text: "foo"},
+			Right: &parse.TextNode{Text: "bar"},
+		}},
+		{`simple NOT`, "NOT (default = ?)", []interface{}{"foo"}, &parse.NotNode{
+			Node: &parse.TextNode{Text: "foo"},
+		}},
+	}
+
+	for _, tcase := range testCases {
+		sql, args := NodesToSQL(tcase.Node, `default`)
+		assert.Equal(t, tcase.SQL, sql)
+		assert.Equal(t, tcase.Args, args)
+	}
+}
